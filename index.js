9807ad343fc43445b2d90019304afe8cf8cfd59a
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2021-01-07 08:04:07 +0900

    Done :)

---
+++ index.js
@@ -0,0 +1,17 @@
+const server = document.currentScript.getAttribute('server');
+const trigger = document.currentScript.getAttribute('trigger');
+const ch = document.currentScript.getAttribute('ch');
+
+function ___makeconn() {
+    var evt = new EventSource(server+"/sse/listen/"+ch);
+    evt.addEventListener(trigger||"reloadpage", function (e) {
+        console.log(e);
+        window.location.reload();
+    }, false);
+    evt.addEventListener('error', function (e) {
+        evt.close();
+        makeconn();
+    }, false);
+}
+
+___makeconn();
